96e2ec01c855e8a66b99f0c066fe611b1a894d3e
Victor Hugo <113782210+vi73458@users.noreply.github.com>
2026-09-16 18:41:02 -0300

    feat(techcare): adiciona estados da ordem de serviço

---
+++ techcare/backend/src/main/java/br/com/victorhugo/techcare/domain/StatusOrdemServico.java
@@ -0,0 +1,11 @@
+package br.com.victorhugo.techcare.domain;
+
+public enum StatusOrdemServico {
+    ABERTA,
+    EM_DIAGNOSTICO,
+    AGUARDANDO_APROVACAO,
+    EM_REPARO,
+    PRONTA,
+    ENTREGUE,
+    CANCELADA
+}
