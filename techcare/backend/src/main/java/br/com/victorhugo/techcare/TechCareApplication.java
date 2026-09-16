828f37e2c40386d411d799c55a54398cc7376276
Victor Hugo <113782210+vi73458@users.noreply.github.com>
2026-09-16 18:40:58 -0300

    feat(techcare): cria classe principal da aplicação

---
+++ techcare/backend/src/main/java/br/com/victorhugo/techcare/TechCareApplication.java
@@ -0,0 +1,12 @@
+package br.com.victorhugo.techcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechCareApplication.class, args);
+    }
+}
